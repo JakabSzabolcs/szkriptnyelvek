a787ec4db3ddb062d301bf1d926cb912d7746810
szabesz <78788328+JakabSzabolcs@users.noreply.github.com>
2023-02-27 17:03:25 +0100

    Create listak.py

---
+++ 3/listak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+# d = [1,2,3]
+# ennek egy másolatát úgy hozzuk létre hogy e = d[:] vagy van copy() metódusa mivel a lista is egy objektum
+# a másolatban a változások nem befolyásolják az eredeti listát
+
+# .append() metódus hozzáad egy elemet a lista végéhez
+# help()
