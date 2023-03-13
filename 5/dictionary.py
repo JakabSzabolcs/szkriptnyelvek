764f44c95175e075d93564873af1e0c6711c4514
szabesz <78788328+JakabSzabolcs@users.noreply.github.com>
2023-03-13 17:32:09 +0100

    Create dictionary.py

---
+++ 5/dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# kulcs érték tárolására használjuk a dictionary-t
+# a kulcsok egyediek
+
+def main():
+    empty_Dict = {}
+    print(empty_Dict)
+    animal_Dict = {"kutya": "dog", "macska": "cat", "cica": "cat"}
+
+
+if __name__ == "__main__":
+    main()
