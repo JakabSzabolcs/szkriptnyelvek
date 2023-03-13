7e6f377cff080ee480996ab1f47a33c6f9f35ffb
szabesz <78788328+JakabSzabolcs@users.noreply.github.com>
2023-03-13 17:13:24 +0100

    kardinalitás XDDDDDDDDDD

---
+++ 5/set.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# a set egy halmaz, amiben csak egyedi elemek lehetnek
+# a set() függvény létrehoz egy üres halmazt
+
+def main():
+    kosar = ['alma', 'körte', 'alma', 'szilva', 'körte']
+    kosar = set(kosar)
+    print(kosar)
+    # kardinalitás (hány elem van a halmazban)
+
+
+if __name__ == "__main__":
+    main()
