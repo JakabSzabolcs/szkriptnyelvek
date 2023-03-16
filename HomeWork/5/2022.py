48fb29f6139bf763736afa92d5914dc5e28a5e15
szabesz <jakab.szabolcs226@gmail.com>
2023-03-16 09:39:44 +0100

    Create 2022.py

---
+++ HomeWork/5/2022.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def distance(chara, charb):
+    return str(abs(ord(chara) - ord(charb)))
+
+def main():
+    ketezerhuszonketto = (distance('a', 'c') +  distance('a', 'a') + distance('a', 'c') +distance('a', 'c') )
+    print(ketezerhuszonketto)
+
+if __name__ == "__main__":
+    main()
