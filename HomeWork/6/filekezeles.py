a8254af7ffc294784c55c1ffb8a6d34d5b462dcd
szabesz <jakab.szabolcs226@gmail.com>
2023-03-22 11:38:18 +0100

    Update filekezeles.py

--- HomeWork/6/filekezeles.py
+++ HomeWork/6/filekezeles.py
@@ -1,16 +1,16 @@
 
 
-#open /2/string1.py delete all comments and save it to string1_clean.py to this folder
-
+#open string1.py delete all comments and save it to string1_clean.py to this folder
 
 def main():
-    f = open("string1.py", "r")
-    lines = f.readlines()
-    for line in lines:
-        if line.startswith("#"):
-            continue
-        print(line, end="")
-    f.close()
+    file = open("string1.py", "r")
+    file2 = open("string1_clean.py", "w")
+    for line in file:
+        if line[0] != "#":
+            file2.write(line)
+    file.close()
+    file2.close()
+
 
 if __name__ == "__main__":
     main()
