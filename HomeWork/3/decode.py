18e67225057345204cbfa9cf3724cc881a3e1af6
szabesz <78788328+JakabSzabolcs@users.noreply.github.com>
2023-03-22 12:19:28 +0100

    Create decode.py

---
+++ HomeWork/3/decode.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def decode(s):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    dictionary = {}
+    for i in range(len(alphabet)):
+        dictionary[alphabet[i]] = alphabet[(i + 2) % 26]
+    print(dictionary)
+    decoded = ""
+    for i in s:
+        if i in dictionary:
+            decoded += dictionary[i]
+        else:
+            decoded += i
+    return decoded
+
+
+if __name__ == '__main__':
+    message = """"Cbcq Dgyk!
+
+Dmeybh kce cew yrwyg hmrylyaqmr:
+rylsjb kce y Nwrfml npmepykmxyqg lwcjtcr!
+
+Aqmimjjyi:
+
+Ynyb"""
+    print(decode(message))
