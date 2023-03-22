2db599f93ae32de8a4a7ed2c2564e7a139676771
szabesz <78788328+JakabSzabolcs@users.noreply.github.com>
2023-03-22 12:33:11 +0100

    Update a-z.py

--- HomeWork/6/a-z.py
+++ HomeWork/6/a-z.py
@@ -1,8 +1,9 @@
-#Írjanak egy a-z.py nevű szkriptet, amit lefuttatva megkapjuk az angol ábécé kisbetűit a-tól z-ig. Tegyünk egy szimbolikus linket a szkriptre z-a.py néven. A z-a.py -t futtatva viszont az angol ábécé kisbetűit fordítva szeretnénk megkapni, vagyis z-től a-ig
-
+#!/usr/bin/env python3
+# encoding: utf-8
 
 def main():
     return "".join([chr(i) for i in range(97, 123)])
 
+
 if __name__ == "__main__":
-    main()
+    print(main())
