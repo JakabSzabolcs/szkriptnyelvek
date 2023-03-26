36d6afe84ddac993c856f00ba991eea1c40e1638
szabesz <78788328+JakabSzabolcs@users.noreply.github.com>
2023-03-26 18:31:14 +0200

    fullos

--- PROBAZH/harmadik.py
+++ PROBAZH/harmadik.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
+from sys import argv
+import sys
+
+
+def karakter_szamlalo(sztring):
+    szotar = {}
+    for karakter in sztring:
+        if karakter in szotar:
+            szotar[karakter] += 1
+        else:
+            szotar[karakter] = 1
+
+    return szotar
+
 
 def main():
-    pass
+    if len(argv) != 2:
+        print(
+            "Hiba! A program csak egy parancssori argumentumot fogad el!", file=sys.stderr)
+        exit()
+    eredmeny = karakter_szamlalo(argv[1])
+    for par in eredmeny:
+        print(par, "-", eredmeny[par])
 
 
 if __name__ == "__main__":
--- PROBAZH/masodik.py
+++ PROBAZH/masodik.py
@@ -1,7 +1,44 @@
 #!/usr/bin/env python3
 
+def Hangrend(szo):
+    # csak angol abc betuket tartalmaz
+    mely = "aou"
+    magas = "ei"
+    melycount = 0
+    magascount = 0
+    for s in szo:
+        if s in mely:
+            melycount += 1
+        if s in magas:
+            magascount += 1
+    if melycount > 0 and magascount == 0:
+        return 1
+    elif magascount > 0 and melycount == 0:
+        return 2
+    elif magascount > 0 and melycount > 0:
+        return 3
+    else:
+        return 4
+
+
 def main():
-    pass
+    szavak = open("words.txt", "r")
+    mely = 0
+    magas = 0
+    vegyes = 0
+    for sor in szavak:
+        szo = sor.strip()
+        if Hangrend(szo) == 1:
+            mely += 1
+        elif Hangrend(szo) == 2:
+            magas += 1
+        elif Hangrend(szo) == 3:
+            vegyes += 1
+        elif Hangrend(szo) == 4:
+            continue
+    print("Magas hangrendű szavak - ", magas)
+    print("Mély hangrendű szavak - ", mely)
+    print("Vegyes hangrendű szavak - ", vegyes)
 
 
 if __name__ == "__main__":
