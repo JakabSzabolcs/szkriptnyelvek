d65afd1b4ca718a22f71710cf73adaea8d266834
szabesz <78788328+JakabSzabolcs@users.noreply.github.com>
2023-04-03 17:10:14 +0200

    Update ora.py

--- 8/ora.py
+++ 8/ora.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 class Allatok:
     pass
 
@@ -15,6 +16,14 @@ class Hello:
 
     def create_name(self, name):
         self.name = name
+
+
+class Bag:
+    def __init__(self):
+        self.data = []
+
+    def add(self, x):
+        self.data.append(x)
 
 
 def main():
