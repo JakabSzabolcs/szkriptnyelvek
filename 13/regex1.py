3ecfaa34d89ab9db1db5c3515645bd52e6ecd1ac
szabesz <jakab.szabolcs226@gmail.com>
2023-05-08 14:53:14 +0200

    Create regex1.py

---
+++ 13/regex1.py
@@ -0,0 +1,34 @@
+
+import re
+
+
+def find(keresett, s):
+    m = re.search(keresett, s)
+    if m:
+        print("Találat: ", m.group())
+        print("Kezdete: ", m.start())
+        print("Vége: ", m.end())
+        print("Kezdete és vége: ", m.span())
+    else:
+        print("Nincs találat!")
+
+
+def main():
+    s = "Reguláris kifejezés"
+    keresett = "kif"
+    m = re.search(keresett, s)
+    print(m)
+    print(m.group())
+    print(m.span())
+    find(keresett, s)
+
+    m2 = re.search("cica", s)
+    print(m2)
+    if m2:
+        print(m2.group())
+    
+
+
+if __name__ == "__main__":
+    main()
+
